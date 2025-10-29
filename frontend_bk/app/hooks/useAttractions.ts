3a1fb2bc715cda64d5dbfcc3c955a81d28eba647
Gloireski <belemgloirebekoutou8@gmail.com>
2025-10-29 09:18:19 +0100

    remove unuseful stuff

--- frontend_bk/app/hooks/useAttractions.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import api from '../services/api';
-
-export const useAttractions = () => {
-  return useQuery(['attractions'], async () => {
-    const { data } = await api.get('attractions/');
-    return data;
-  });
-};
